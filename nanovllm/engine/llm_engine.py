ee375ea65c9850f2917cd4efc416a4800fd03fbd
HSpark <46282235+Hg-Spark@users.noreply.github.com>
2026-09-21 21:17:49 +0800

    refactor(sequence): keep block size in KV owners

--- nanovllm/engine/llm_engine.py
+++ nanovllm/engine/llm_engine.py
@@ -41,7 +41,6 @@ class LLMEngine:
             if key in config_fields
         }
         config = Config(model, **config_kwargs)
-        Sequence.block_size = config.kvcache_block_size
 
         self.model_runner = ModelRunner(config)
         self.tokenizer = AutoTokenizer.from_pretrained(
