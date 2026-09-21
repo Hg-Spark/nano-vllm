02e01c2a26b5cd2ada31f158bbae5e495a957719
HSpark <46282235+Hg-Spark@users.noreply.github.com>
2026-09-21 15:57:39 +0800

    hybrid: carry packed sequence metadata in context

--- nanovllm/utils/context.py
+++ nanovllm/utils/context.py
@@ -12,15 +12,43 @@ class Context:
     slot_mapping: torch.Tensor | None = None
     context_lens: torch.Tensor | None = None
     block_tables: torch.Tensor | None = None
+    seq_ids: tuple[int, ...] = ()
+    seq_lens: tuple[int, ...] = ()
+
 
 _CONTEXT = Context()
+
 
 def get_context():
     return _CONTEXT
 
-def set_context(is_prefill, cu_seqlens_q=None, cu_seqlens_k=None, max_seqlen_q=0, max_seqlen_k=0, slot_mapping=None, context_lens=None, block_tables=None):
+
+def set_context(
+    is_prefill,
+    cu_seqlens_q=None,
+    cu_seqlens_k=None,
+    max_seqlen_q=0,
+    max_seqlen_k=0,
+    slot_mapping=None,
+    context_lens=None,
+    block_tables=None,
+    seq_ids=(),
+    seq_lens=(),
+):
     global _CONTEXT
-    _CONTEXT = Context(is_prefill, cu_seqlens_q, cu_seqlens_k, max_seqlen_q, max_seqlen_k, slot_mapping, context_lens, block_tables)
+    _CONTEXT = Context(
+        is_prefill=is_prefill,
+        cu_seqlens_q=cu_seqlens_q,
+        cu_seqlens_k=cu_seqlens_k,
+        max_seqlen_q=max_seqlen_q,
+        max_seqlen_k=max_seqlen_k,
+        slot_mapping=slot_mapping,
+        context_lens=context_lens,
+        block_tables=block_tables,
+        seq_ids=tuple(seq_ids),
+        seq_lens=tuple(seq_lens),
+    )
+
 
 def reset_context():
     global _CONTEXT
