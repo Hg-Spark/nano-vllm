d7184e7d41ec719b890383b1a9e143f0f7a4e74f
HSpark <46282235+Hg-Spark@users.noreply.github.com>
2026-09-21 21:17:33 +0800

    refactor(sequence): remove KV block geometry from requests

--- nanovllm/engine/sequence.py
+++ nanovllm/engine/sequence.py
@@ -12,7 +12,6 @@ class SequenceStatus(Enum):
 
 
 class Sequence:
-    block_size = 256
     counter = count()
 
     def __init__(
@@ -62,26 +61,6 @@ class Sequence:
     @property
     def completion_token_ids(self):
         return self.token_ids[self.num_prompt_tokens:]
-
-    @property
-    def num_blocks(self):
-        return (
-            self.num_tokens + self.block_size - 1
-        ) // self.block_size
-
-    @property
-    def last_block_num_tokens(self):
-        return (
-            self.num_tokens
-            - (self.num_blocks - 1) * self.block_size
-        )
-
-    def block(self, i):
-        if not 0 <= i < self.num_blocks:
-            raise IndexError(i)
-        return self.token_ids[
-            i * self.block_size:(i + 1) * self.block_size
-        ]
 
     def append_token(self, token_id: int):
         self.token_ids.append(token_id)
